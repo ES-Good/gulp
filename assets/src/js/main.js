8597d4920d21167892d43ea173cffd61049df954
unknown <evgenshtanko@ya.ru>
2021-03-23 19:38:45 +0400

    t

---
+++ assets/src/js/main.js
@@ -0,0 +1,24 @@
+
+
+// Импортируем Popper
+//= ../../../node_modules/popper.js/dist/umd/popper.js
+
+// Импортируем необходимые js-файлы Bootstrap 5
+
+//= ../../../node_modules/bootstrap/js/dist/alert.js
+//= ../../../node_modules/bootstrap/js/dist/base-component.js
+//= ../../../node_modules/bootstrap/js/dist/button.js
+//= ../../../node_modules/bootstrap/js/dist/carousel.js
+//= ../../../node_modules/bootstrap/js/dist/collapse.js
+//= ../../../node_modules/bootstrap/js/dist/dropdown.js
+//= ../../../node_modules/bootstrap/js/dist/modal.js
+//= ../../../node_modules/bootstrap/js/dist/popover.js
+//= ../../../node_modules/bootstrap/js/dist/scrollspy.js
+//= ../../../node_modules/bootstrap/js/dist/tab.js
+//= ../../../node_modules/bootstrap/js/dist/toast.js
+//= ../../../node_modules/bootstrap/js/dist/tooltip.js
+
+
+
+// Импортируем другие js-файлы
+//= my.js
