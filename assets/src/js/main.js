fff59bc93de6dbd1d544f262c468bcfdc2d6ef73
ES-Good <46601455+ES-Good@users.noreply.github.com>
2021-05-17 17:13:22 +0400

    't'

--- assets/src/js/main.js
+++ assets/src/js/main.js
@@ -5,18 +5,8 @@
 
 // Импортируем необходимые js-файлы Bootstrap 5
 
-//= ../../../node_modules/bootstrap/js/dist/alert.js
-//= ../../../node_modules/bootstrap/js/dist/base-component.js
-//= ../../../node_modules/bootstrap/js/dist/button.js
-//= ../../../node_modules/bootstrap/js/dist/carousel.js
-//= ../../../node_modules/bootstrap/js/dist/collapse.js
-//= ../../../node_modules/bootstrap/js/dist/dropdown.js
-//= ../../../node_modules/bootstrap/js/dist/modal.js
-//= ../../../node_modules/bootstrap/js/dist/popover.js
-//= ../../../node_modules/bootstrap/js/dist/scrollspy.js
-//= ../../../node_modules/bootstrap/js/dist/tab.js
-//= ../../../node_modules/bootstrap/js/dist/toast.js
-//= ../../../node_modules/bootstrap/js/dist/tooltip.js
+//= ../../../node_modules/bootstrap/dist/js/bootstrap.min.js
+
 
 
 
